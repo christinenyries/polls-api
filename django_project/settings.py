0f31faa82da97a68ab627c6dea3f8432d73cd752
Christine Nyries Yambao <yambao.cn@gmail.com>
2023-02-20 14:50:32 +0800

    fix: missing comma

--- django_project/settings.py
+++ django_project/settings.py
@@ -62,7 +62,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     # must be above CommonMiddleware
-    "whitenoise.middleware.WhiteNoiseMiddleware"
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
